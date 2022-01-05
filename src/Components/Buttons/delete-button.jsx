ac6ddeb934ba9d2bdec8e8a5981e2de460b04d84
Zack Zellto <zacharyzellto@gmail.com>
2022-01-05 14:17:20 -0700

    new UI update

--- src/Components/Buttons/delete-button.jsx
+++ src/Components/Buttons/delete-button.jsx
@@ -1,2 +1,11 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import { Button } from "react-bootstrap";
+
+const submitButton = () => {
+        return(
+            <Submit>
+                Submit!
+            </Submit>
+        )
+    }
--- src/Components/Header/expense-header.jsx
+++ src/Components/Header/expense-header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Logo from "../../Components/Header/logo.png"
 
 function Header() {
-    return <img src={Logo} alt="Logo" />;
+    return <img src={Logo} alt="Logo" name="wheel"/>;
   }
   
   export default Header;
